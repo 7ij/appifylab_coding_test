684718c449fb1050253410a38d300c4d30c0522d
Md. Ashiqur Rahman Naeem <nayeem2021@outlook.com>
2026-03-16 19:07:18 +0600

    feat: register coaching program API and repository in dependency injection

--- lib/di/repository_module.dart
+++ lib/di/repository_module.dart
@@ -1,9 +1,15 @@
 import 'package:appifylab_coding_test/data/repository/auth_repository_impl.dart';
+import 'package:appifylab_coding_test/data/repository/coaching_program_repository_impl.dart';
 import 'package:appifylab_coding_test/di/injection_container.dart';
 import 'package:appifylab_coding_test/domain/repository/auth_repository.dart';
+import 'package:appifylab_coding_test/domain/repository/coaching_program_repository.dart';
 
 void setupRepositoryModule() {
   getIt.registerLazySingleton<AuthRepository>(
-    () => AuthRepositoryImpl(authApi: getIt(), identityLocalDataSource: getIt()),
+    () =>
+        AuthRepositoryImpl(authApi: getIt(), identityLocalDataSource: getIt()),
+  );
+  getIt.registerLazySingleton<CoachingProgramRepository>(
+    () => CoachingProgramRepositoryImpl(coachingProgramApi: getIt()),
   );
 }
--- lib/di/use_case_module.dart
+++ lib/di/use_case_module.dart
@@ -1,8 +1,12 @@
 import 'package:appifylab_coding_test/di/injection_container.dart';
+import 'package:appifylab_coding_test/domain/use_case/get_coaching_program_list_use_case.dart';
 import 'package:appifylab_coding_test/domain/use_case/login_with_email_pass_use_case.dart';
 
 void setupUseCaseModule() {
   getIt.registerLazySingleton<LoginWithEmailPassUseCase>(
     () => LoginWithEmailPassUseCase(authRepository: getIt()),
+  );
+  getIt.registerLazySingleton<GetCoachingProgramListUseCase>(
+    () => GetCoachingProgramListUseCase(repository: getIt()),
   );
 }
